533dc9a0884113d85cef173ebcfd1d5bbe8fa351
steven112163 <43545147+steven112163@users.noreply.github.com>
2019-03-13 01:07:52 +0800

    Update startTopo.sh

--- startTopo.sh
+++ startTopo.sh
@@ -8,12 +8,8 @@ cd /run
 test -e dhcpd.pid && sudo rm dhcpd.pid
 test -e dhclient.pid && sudo rm dhclient.pid
 
-sleep 2s
-
 cd /var/lib/dhcp
 sudo chmod -R 777 dhcpd.leases
-
-sleep 2s
 
 cd ~/ID-based-master
 sudo python topo.py
