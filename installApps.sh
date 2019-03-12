628d8ca1026592a34b85052d765975141567444b
steven112163 <43545147+steven112163@users.noreply.github.com>
2019-03-13 01:07:36 +0800

    Update installApps.sh

--- installApps.sh
+++ installApps.sh
@@ -11,11 +11,7 @@ onos-app localhost install target/ibwd-app-1.10.0.oar
 cd ~/ID-based-master/iacl-app
 onos-app localhost install target/iacl-app-1.10.0.oar
 
-sleep 2s
-
 sudo ovs-vsctl set-manager tcp:127.0.0.1:6640
-
-sleep 2s
 
 onos localhost app activate org.onosproject.ovsdb
 onos localhost app activate org.onosproject.drivers.ovsdb
@@ -23,14 +19,10 @@ onos localhost app activate org.ifwd.app
 onos localhost app activate org.ibwd.app
 onos localhost app activate org.iacl.app
 
-sleep 2s
-
 cd ~/ID-based-master
 onos-netcfg localhost net_config.json
 
 echo "*** Finished!!!\n"
-
-sleep 2s
 
 sudo python access_db.py
 
