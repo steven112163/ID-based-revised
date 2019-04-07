0f0e68249e20c01eced540a89d4fae16630ce54e
steven112163 <43545147+steven112163@users.noreply.github.com>
2019-04-07 18:41:33 +0800

    Update db.php

--- Login/db.php
+++ Login/db.php
@@ -4,7 +4,7 @@ $link = mysqli_connect('192.168.44.128','root','root','portal');
 if (!$link) {
   die('Could not connect:' .mysquli_error());
 }
-echo 'Connected successfully';
+// echo 'Connected successfully';
 mysql_query($link, "SET NAMES 'utf8'");
 date_default_timezone_set('Asia/Taipei');	
 ?>
