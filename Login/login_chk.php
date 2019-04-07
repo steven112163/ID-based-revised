3357558636442c2e7430f07577169519fd8f7da5
steven112163 <43545147+steven112163@users.noreply.github.com>
2019-04-07 18:47:27 +0800

    Update login_chk.php

--- Login/login_chk.php
+++ Login/login_chk.php
@@ -14,7 +14,7 @@ else
     $ip = $_SERVER['REMOTE_ADDR'];
 
 $sql = 'SELECT MAC FROM IP_MAC WHERE IP = "'.$ip.'"';
-$result = mysql_query($sql);
+$result = mysql_query($link, $sql);
 $row = mysql_fetch_assoc($result);
 
 $mac = $row['MAC'];
@@ -23,7 +23,7 @@ $account = isset($_POST["user_name"]) ? $_POST["user_name"] : $_GET["user_name"]
 $password = isset($_POST["user_password"]) ? $_POST["user_password"] : $_GET["user_password"];
 
 $sql = 'SELECT User_ID, Group_ID, Password FROM User WHERE Account = "'.$account.'"';
-$result = mysql_query($sql);
+$result = mysql_query($link, $sql);
 $row = mysql_fetch_assoc($result);
 $record_count = mysql_num_rows($result);
 
@@ -32,6 +32,7 @@ if($record_count<1){
     echo 'fail';  
 }
 else{
+    $_SESSION['username'] = $username;
     $user_id = $row['User_ID']; 
     $group_id = $row['Group_ID'];
     $correct_passwd = $row['Password'];
@@ -41,10 +42,11 @@ else{
         echo 'fail';
     }
     else{
-        $sql = 'UPDATE Registered_MAC SET Enable=true, User_ID = "'.$user_id.'", Group_ID = "'.$group_id.'" WHERE MAC= "'.$mac.'"';
-        $result = mysql_query($sql);
+        //$sql = 'UPDATE Registered_MAC SET Enable=true, User_ID = "'.$user_id.'", Group_ID = "'.$group_id.'" WHERE MAC= "'.$mac.'"';
+        $sql = "INSERT INTO `Registered_MAC` (`Enable`, User_ID, Group_ID, MAC) VALUES (true, "'.$user_id.'", "'.$group_id.'", "'.$mac.'"') ON DUPLICATE KEY UPDATE User_ID = "'.$user_id.'",Group_ID = "'.$group_id.'"", `Enable`=true;
+        $result = mysql_query($link, $sql);
 
-        $data = array("mac" => $mac, "userId" => $user_id, "groupId" => $group_id);
+        /*$data = array("mac" => $mac, "userId" => $user_id, "groupId" => $group_id);
         $data_json = json_encode($data);
         $header = array('Content-Type: application/json', 'Accept: application/json');
 
@@ -56,7 +58,7 @@ else{
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_exec($curl);
-        curl_close($curl);
+        curl_close($curl);*/
             
         echo 'success';
     }
