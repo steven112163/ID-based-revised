63d0b1a37deaf9a0d727e62723ed34bbb96d7ef1
steven112163 <steven112163@gmail.com>
2019-05-05 22:45:40 +0800

    Modify install shell to this directory

--- installApps.sh
+++ installApps.sh
@@ -4,11 +4,11 @@
 # History:
 # 	2019/3/5 Steven Yuan create and test
 
-cd ~/ID-based-master/ibwd-app
+cd ~/ID-based-revised/ibwd-app
 onos-app localhost install target/ibwd-app-1.10.0.oar
-cd ~/ID-based-master/ifwd-app
+cd ~/ID-based-revised/ifwd-app
 onos-app localhost install target/ifwd-app-1.10.0.oar
-cd ~/ID-based-master/iacl-app
+cd ~/ID-based-revised/iacl-app
 onos-app localhost install target/iacl-app-1.10.0.oar
 
 sudo ovs-vsctl set-manager tcp:127.0.0.1:6640
@@ -22,7 +22,7 @@ onos localhost app activate org.ibwd.app
 onos localhost app activate org.ifwd.app
 onos localhost app activate org.iacl.app
 
-cd ~/ID-based-master
+cd ~/ID-based-revised
 onos-netcfg localhost new_net_config.json
 
 echo "*** Finished!!!\n"
